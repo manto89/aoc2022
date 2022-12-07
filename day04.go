a397e38470780d40aa864dc38b8d927a339647e0
manto89 <manto89@gmail.com>
2022-12-07 18:50:28 +0100

    implement day04 part1

--- day04.go
+++ day04.go
@@ -1,12 +1,59 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/manto89/aoc2022/common"
+)
+
 type Day04 struct{
 	inputPath string
 }
 
 func (d *Day04) executePart1() string{
-	return "Not implemented"
+	lines, err := common.ReadAllLines(d.inputPath)
+	if err != nil {
+        return fmt.Sprintf("open file error: %v", err)
+	}
+	sameSections := 0
+	for i, line := range(lines){
+		splittedLine := strings.Split(line, ",")
+		if len(splittedLine) < 2 {
+			return fmt.Sprintf("Unable to decode elves in line %d", i)
+		}
+		firstElf := strings.Split(splittedLine[0],"-")
+		if len(firstElf) < 2 {
+			return fmt.Sprintf("Unable to decode first elf in line %d", i)
+		}
+		secondElf := strings.Split(splittedLine[1],"-")
+		if len(secondElf) < 2 {
+			return fmt.Sprintf("Unable to decode second elf in line %d", i)
+		}
+		firstElfFirstSection, err := strconv.Atoi(firstElf[0])
+		if err != nil {
+			return fmt.Sprintf("Unable to decode sections in line %d", i)
+		}
+		firstElfSecondSection, err := strconv.Atoi(firstElf[1])
+		if err != nil {
+			return fmt.Sprintf("Unable to decode sections in line %d", i)
+		}
+		secondElfFirstSection, err := strconv.Atoi(secondElf[0])
+		if err != nil {
+			return fmt.Sprintf("Unable to decode sections in line %d", i)
+		}
+		secondElfSecondSection, err := strconv.Atoi(secondElf[1])
+		if err != nil {
+			return fmt.Sprintf("Unable to decode sections in line %d", i)
+		}
+		if (firstElfFirstSection >= secondElfFirstSection && firstElfSecondSection <= secondElfSecondSection) ||
+		   (secondElfFirstSection >= firstElfFirstSection && secondElfSecondSection <= firstElfSecondSection) {
+			sameSections += 1
+		}
 
+	}
+	return fmt.Sprintf("Elves couple doing the same sections are %d", sameSections)
 }
 func (d *Day04) executePart2() string{
 	return "Not implemented"
--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 		1: &Day01{inputPath: "inputs/day01.txt"},
 		2: &Day02{inputPath: "inputs/day02.txt"},
 		3: &Day03{inputPath: "inputs/day03.txt"},
-		4: &Day03{inputPath: "inputs/day04.txt"},
+		4: &Day04{inputPath: "inputs/day04.txt"},
 	}	
 	dayToExecute, ok := days[dayNum]
 	if !ok {
