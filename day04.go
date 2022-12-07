bdcb14513dc7f6dc32b6c9024cd4a187617d3ab6
manto89 <manto89@gmail.com>
2022-12-07 18:35:18 +0100

    init day04

---
+++ day04.go
@@ -0,0 +1,14 @@
+package main
+
+type Day04 struct{
+	inputPath string
+}
+
+func (d *Day04) executePart1() string{
+	return "Not implemented"
+
+}
+func (d *Day04) executePart2() string{
+	return "Not implemented"
+
+}
