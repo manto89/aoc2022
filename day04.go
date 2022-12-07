af9c92ba9f3b4b50a6470f5b96e4126d1548dd65
manto89 <manto89@gmail.com>
2022-12-07 18:57:45 +0100

    refactor extract function for day04 part2

--- day04.go
+++ day04.go
@@ -19,33 +19,9 @@ func (d *Day04) executePart1() string{
 	}
 	sameSections := 0
 	for i, line := range(lines){
-		splittedLine := strings.Split(line, ",")
-		if len(splittedLine) < 2 {
-			return fmt.Sprintf("Unable to decode elves in line %d", i)
-		}
-		firstElf := strings.Split(splittedLine[0],"-")
-		if len(firstElf) < 2 {
-			return fmt.Sprintf("Unable to decode first elf in line %d", i)
-		}
-		secondElf := strings.Split(splittedLine[1],"-")
-		if len(secondElf) < 2 {
-			return fmt.Sprintf("Unable to decode second elf in line %d", i)
-		}
-		firstElfFirstSection, err := strconv.Atoi(firstElf[0])
-		if err != nil {
-			return fmt.Sprintf("Unable to decode sections in line %d", i)
-		}
-		firstElfSecondSection, err := strconv.Atoi(firstElf[1])
-		if err != nil {
-			return fmt.Sprintf("Unable to decode sections in line %d", i)
-		}
-		secondElfFirstSection, err := strconv.Atoi(secondElf[0])
-		if err != nil {
-			return fmt.Sprintf("Unable to decode sections in line %d", i)
-		}
-		secondElfSecondSection, err := strconv.Atoi(secondElf[1])
-		if err != nil {
-			return fmt.Sprintf("Unable to decode sections in line %d", i)
+		firstElfFirstSection, firstElfSecondSection, secondElfFirstSection, secondElfSecondSection, err := decodeElvesSections(line, i)
+		if err != nil{
+			return err.Error()
 		}
 		if (firstElfFirstSection >= secondElfFirstSection && firstElfSecondSection <= secondElfSecondSection) ||
 		   (secondElfFirstSection >= firstElfFirstSection && secondElfSecondSection <= firstElfSecondSection) {
@@ -58,4 +34,36 @@ func (d *Day04) executePart1() string{
 func (d *Day04) executePart2() string{
 	return "Not implemented"
 
+}
+
+func decodeElvesSections(line string, i int) (int, int, int, int, error){
+		splittedLine := strings.Split(line, ",")
+		if len(splittedLine) < 2 {
+			return 0,0,0,0,fmt.Errorf("unable to decode elves in line %d", i)
+		}
+		firstElf := strings.Split(splittedLine[0],"-")
+		if len(firstElf) < 2 {
+			return 0,0,0,0,fmt.Errorf("unable to decode first elf in line %d", i)
+		}
+		secondElf := strings.Split(splittedLine[1],"-")
+		if len(secondElf) < 2 {
+			return 0,0,0,0,fmt.Errorf("unable to decode second elf in line %d", i)
+		}
+		firstElfFirstSection, err := strconv.Atoi(firstElf[0])
+		if err != nil {
+			return 0,0,0,0,fmt.Errorf("unable to decode sections in line %d", i)
+		}
+		firstElfSecondSection, err := strconv.Atoi(firstElf[1])
+		if err != nil {
+			return 0,0,0,0,fmt.Errorf("unable to decode sections in line %d", i)
+		}
+		secondElfFirstSection, err := strconv.Atoi(secondElf[0])
+		if err != nil {
+			return 0,0,0,0,fmt.Errorf("unable to decode sections in line %d", i)
+		}
+		secondElfSecondSection, err := strconv.Atoi(secondElf[1])
+		if err != nil {
+			return 0,0,0,0,fmt.Errorf("unable to decode sections in line %d", i)
+		}
+		return firstElfFirstSection, firstElfSecondSection, secondElfFirstSection, secondElfSecondSection, nil
 }
