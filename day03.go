c136e5c8ea682a8c2e8fd97b03e50e85fe99b271
manto89 <manto89@gmail.com>
2022-12-07 18:20:02 +0100

    implement day03 part1

--- day03.go
+++ day03.go
@@ -1,11 +1,61 @@
 package main
 
+import (
+	"fmt"
+	"github.com/manto89/aoc2022/common"
+)
+
 type Day03 struct{
 	inputPath string
 }
 
 func (d *Day03) executePart1() string{
-	return "Not implemented"
+	lines, err := common.ReadAllLines(d.inputPath)
+	if err != nil {
+        return fmt.Sprintf("open file error: %v", err)
+	}
+	priorityScore := 0
+	for i, line := range(lines){
+		var items [52]bool
+		compartment1 := line[:(len(line)/2)]
+		compartment2 := line[(len(line)/2):]
+
+		for _, char := range(compartment1){
+			priority, err := GetPriority(char)
+			if err != nil {
+				return fmt.Sprintf("Unable to decode character at line %d", i)
+			}
+			items[priority-1] = true
+		}
+		for _, char := range(compartment2){
+			priority, err := GetPriority(char)
+			if err != nil {
+				return fmt.Sprintf("Unable to decode character at line %d", i)
+			}
+			if items[priority-1] {
+				priorityScore += priority
+				break
+
+			}
+
+		}
+
+	}
+	return fmt.Sprintf("Priority score is %d", priorityScore)
+
+}
+
+func GetPriority(c rune) (int, error) {
+	priority := 0
+	ascii := int(c)
+	if ascii > 96 && ascii < 123 {
+		priority = ascii - 96
+	} else if ascii > 64 && ascii < 91{
+		priority = ascii - 38
+	} else {
+		return 0, fmt.Errorf("unable to decode character %c", c)
+	}
+	return priority, nil
 }
 
 func (d *Day03) executePart2() string{
